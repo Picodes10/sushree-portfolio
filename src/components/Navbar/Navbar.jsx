52741c2d28102540bb4e0a2aae183bfcec46a91e
Sushree Manaswini Biswal <biswalsushree000@gmail.com>
2024-06-28 23:59:01 +0530

    Done

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+       <h2>Sushree.</h2>
+       <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>connect</li>
+       </ul>
+    </div>
+  )
+}
+
+export default Navbar
